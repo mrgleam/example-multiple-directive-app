8b2875e65b6d356fd6cd6d124c09fc19bc4d0d51
Santi <santi@odds.team>
2022-02-19 00:27:51 +0700

    feat: add demo code for use multiple directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'example-multiple-directive-app';
+export class AppComponent implements OnInit {
+
+  title = 'example-multiple-directive-app';  
+  public nestedForm: FormGroup = new FormGroup({
+    basicInfo: new FormControl("")
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.nestedForm.valueChanges.subscribe(v => console.log(v))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxEnglishOnlyModule } from 'ngx-english-only';
+import { RemoveNewLineModule } from 'ngx-remove-new-line';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyInputComponent } from './my-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyInputComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxEnglishOnlyModule,
+    RemoveNewLineModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
